d15e566115d47249eadcf9ed840ec84953a5ea71
Raoul <raoul@doin-good.com>
2023-06-28 20:50:39 +0200

    Finish ref impl for tic tac toe

--- eks/tic_tac_toe_eks22.py
+++ eks/tic_tac_toe_eks22.py
@@ -1,10 +1,19 @@
-# Spielablauf:
-# - 1) Spielfeld anzeigen - OK
-# - 2) Aktuellen Spieler anzeigen - OK
-# - 3) Spieler waehlt Feld aus - OK
-# - 4) Prüfen, ob das Spiel vorbei ist
-#   - wenn ja: Gewinner anzeigen und Spielfeld anzeigen
-#   - wenn nein: zurueck zu 1)
+# KI:
+# 1. Wenn leer, dann Spielfeld 5
+#   - Alle Felder durchgehen, wenn keines davon 'X' oder 'O' ist, ist das Feld leer. - OK
+
+# 2. Wenn ich gewinnen kann, nimm das Feld, das gewinnt.
+#   - Alle Spalten, alle Reihen, alle Diagonalen überprüfen:
+#        - Habe ich schon zwei Felder belegt und ist das dritte Feld noch frei?
+#        - Wenn ja, nimm das noch freie Feld.
+
+# 3. Wenn der Gegner im nächsten Zug gewinnt, nimm das Feld, das dies verhindert
+#   - Alle Spalten, alle Reihen, alle Diagonalen überprüfen:
+#        - Hat der Gegner schon zwei Felder belegt und ist das dritte Feld noch frei?
+#        - Wenn ja, nimm das noch freie Feld.
+
+# 4. Wähle ein zufälliges Feld.
+#   - Bestimme alle Felder, die noch frei sind, wähle zufällig eines aus. - OK
 
 import random
 
@@ -19,62 +28,102 @@ feld08 = "8"
 feld09 = "9"
 
 
-def reihe1():
-    global feld01, feld02, feld03
-    return [feld01, feld02, feld03]
+def freie_felder():
+    global feld01, feld02, feld03, feld04, feld05, feld06, feld07, feld08, feld09
+    felder = [feld01, feld02, feld03, feld04,
+              feld05, feld06, feld07, feld08, feld09]
+    leere_felder = []
+    for feld in felder:
+        if feld != "X" and feld != "O":
+            leere_felder.append(feld)
+    return leere_felder
 
 
-def reihe2():
-    global feld04, feld05, feld06
-    return [feld04, feld05, feld06]
-
-
-def reihe3():
-    global feld07, feld08, feld09
-    return [feld07, feld08, feld09]
-
-
-def spalte1():
-    global feld01, feld04, feld07
-    return [feld01, feld04, feld07]
-
-
-def spalte2():
-    global feld02, feld05, feld08
-    return [feld02, feld05, feld08]
-
-
-def spalte3():
-    global feld03, feld06, feld09
-    return [feld03, feld06, feld09]
-
-
-def diagonale1():
-    global feld01, feld05, feld09
-    return [feld01, feld05, feld09]
-
-
-def diagonale2():
-    global feld03, feld05, feld07
-    return [feld03, feld05, feld07]
-
-
-def alle_dreier():
-    return [reihe1(), reihe2(), reihe3(), spalte1(), spalte2(), spalte3(), diagonale1(), diagonale2()]
-
-
-def select_single_field_or_none(fields, player):
-    if fields[0] == player and fields[1] == player:
-        return fields[2]
-    if fields[1] == player and fields[2] == player:
-        return fields[0]
-    if fields[0] == player and fields[2] == player:
-        return fields[1]
+def finde_einzelnes_freies_feld(felder, spieler):
+    anzahl = felder.count(spieler)
+    if anzahl == 2:
+        for feld in felder:
+            if feld != spieler and feld in freie_felder():
+                return feld
     return None
 
 
+def ki_auswahl():
+    leere_felder = freie_felder()
+
+    # Regel 1
+    if len(leere_felder) == 9:
+        return "5"
+
+    # Regel 2
+    reihe1 = [feld01, feld02, feld03]
+    reihe2 = [feld04, feld05, feld06]
+    reihe3 = [feld07, feld08, feld09]
+    spalte1 = [feld01, feld04, feld07]
+    spalte2 = [feld02, feld05, feld08]
+    spalte3 = [feld03, feld06, feld09]
+    diagonale1 = [feld01, feld05, feld09]
+    diagonale2 = [feld03, feld05, feld07]
+    alle_dreier = [reihe1, reihe2, reihe3, spalte1,
+                   spalte2, spalte3, diagonale1, diagonale2]
+
+    feld_auswahl = finde_einzelnes_freies_feld(reihe1, "O")
+    if feld_auswahl != None:
+        return feld_auswahl
+
+    return random.choice(leere_felder)
+
+
+def ki_auswahl_ref():
+    leere_felder = freie_felder()
+
+    # Regel 1
+    if len(leere_felder) == 9:
+        return "5"
+
+    if len(leere_felder) == 1:
+        return leere_felder[0]
+
+    # Regel 2
+    reihe1 = [feld01, feld02, feld03]
+    reihe2 = [feld04, feld05, feld06]
+    reihe3 = [feld07, feld08, feld09]
+    spalte1 = [feld01, feld04, feld07]
+    spalte2 = [feld02, feld05, feld08]
+    spalte3 = [feld03, feld06, feld09]
+    diagonale1 = [feld01, feld05, feld09]
+    diagonale2 = [feld03, feld05, feld07]
+    alle_dreier = [reihe1, reihe2, reihe3, spalte1,
+                   spalte2, spalte3, diagonale1, diagonale2]
+
+    # Prüfe, ob KI gewinnen kann.
+    for dreier in alle_dreier:
+        feld_auswahl = finde_einzelnes_freies_feld(dreier, "O")
+        if feld_auswahl != None:
+            return feld_auswahl
+
+    # Prüfe, ob Gegner gewinnen kann.
+    for dreier in alle_dreier:
+        feld_auswahl = finde_einzelnes_freies_feld(dreier, "X")
+        if feld_auswahl != None:
+            return feld_auswahl
+
+    return random.choice(leere_felder)
+
+
+print(finde_einzelnes_freies_feld(["3", "O", "O"], "O"))
+
+
+# Spielablauf:
+# - 1) Spielfeld anzeigen - OK
+# - 2) Aktuellen Spieler anzeigen - OK
+# - 3) Spieler waehlt Feld aus - OK
+# - 4) Prüfen, ob das Spiel vorbei ist
+#   - wenn ja: Gewinner anzeigen und Spielfeld anzeigen
+#   - wenn nein: zurueck zu 1)
+#
+
 def felder_belegt():
-    global feld01, feld02, feld03, feld04, feld05, feld06, feld07, feld08, feld09
     return feld01 != "1" and feld02 != "2" and feld03 != "3" and \
         feld04 != "4" and feld05 != "5" and feld06 != "6" and \
         feld07 != "7" and feld08 != "8" and feld09 != "9"
@@ -127,81 +176,60 @@ def gebe_spielfeld_aus():
     print(feld07, "|", feld08, "|", feld09)
 
 
-def spielzug():
+def spieler_auswahl():
+    while True:
+        feld_auswahl = input("Bitte Feld auswaehlen (1-9): ")
+        if feld_auswahl in freie_felder():
+            return feld_auswahl
+        else:
+            print("Ungültige Auswahl!")
+
+
+def setze_auswahl(feld_auswahl, spieler):
     global feld01, feld02, feld03, feld04, feld05, feld06, feld07, feld08, feld09
-    feld_auswahl = input("Bitte Feld auswaehlen (1-9): ")
-    auswahl_ok = False
     if feld_auswahl == "1":
         if feld01 == "1":
             feld01 = spieler
-            auswahl_ok = True
     if feld_auswahl == "2":
         if feld02 == "2":
             feld02 = spieler
-            auswahl_ok = True
     if feld_auswahl == "3":
         if feld03 == "3":
             feld03 = spieler
-            auswahl_ok = True
     if feld_auswahl == "4":
         if feld04 == "4":
             feld04 = spieler
-            auswahl_ok = True
     if feld_auswahl == "5":
         if feld05 == "5":
             feld05 = spieler
-            auswahl_ok = True
     if feld_auswahl == "6":
         if feld06 == "6":
             feld06 = spieler
-            auswahl_ok = True
     if feld_auswahl == "7":
         if feld07 == "7":
             feld07 = spieler
-            auswahl_ok = True
     if feld_auswahl == "8":
         if feld08 == "8":
             feld08 = spieler
-            auswahl_ok = True
     if feld_auswahl == "9":
         if feld09 == "9":
             feld09 = spieler
-            auswahl_ok = True
-
-
-def ki_auswahl():
-    global feld01, feld02, feld03, feld04, feld05, feld06, feld07, feld08, feld09
-    freie_felder = []
-    if feld01 == "1":
-        freie_felder.append(feld01)
-    if feld02 == "2":
-        freie_felder.append(feld02)
-    if feld03 == "3":
-        freie_felder.append(feld03)
-    if feld04 == "4":
-        freie_felder.append(feld04)
-    if feld05 == "5":
-        freie_felder.append(feld05)
-    if feld06 == "6":
-        freie_felder.append(feld06)
-    if feld07 == "7":
-        freie_felder.append(feld07)
-    if feld08 == "8":
-        freie_felder.append(feld08)
-    if feld09 == "9":
-        freie_felder.append(feld09)
 
 
 spieler = random.choice(["X", "O"])
-print(alle_dreier())
 spiel_vorbei = False
 while not spiel_vorbei:
     gebe_spielfeld_aus()
     gebe_aktuellen_spieler_aus(spieler)
+    feld_auswahl = ""
+
     if spieler == "X":
-        auswahl_ok = spielzug()
+        feld_auswahl = spieler_auswahl()
     else:
-        ki_auswahl()
+        feld_auswahl = ki_auswahl_ref()
+        print("O waehlt:", feld_auswahl)
+
+    setze_auswahl(feld_auswahl, spieler)
 
     if gewonnen():
         print(spieler, "hat gewonnen!")
